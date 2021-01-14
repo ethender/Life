1cfc2502e6f6464a6b1e48855e2ef984b15637dc
ethender <e.talamarla@nexus.com.do>
2021-01-14 14:41:11 +0530

    changing docker support

--- lifebackendserver/src/appconstants.js
+++ lifebackendserver/src/appconstants.js
@@ -2,6 +2,7 @@ var CONSTANTS = {
     SERVERPORT: 3030,
     DATABASE_SERVER: 'mongodb',
     //DATABASE_SERVER: 'localhost',
+    //DATABASE_SERVER: '192.168.0.100',
     DATABASE_PORT: 27017,
     DATABASE:'LifeDev'
     
